cd26e71d2a37b718ced8daadc40b1efdb720d50e
Roberto Reale <rober.reale@gmail.com>
2018-06-08 16:42:45 +0200

    Reformat SQL snippet.

--- chapters/sql/oracle-index-fragmentation-factor.sql
+++ chapters/sql/oracle-index-fragmentation-factor.sql
@@ -4,7 +4,7 @@ SELECT tablespace_name AS TABLESPACE,
 
        nvl(max(bytes) / 1048576, 0) AS largest_chunk,
 
-       100 * ( 1 - nvl(sqrt(max(blocks) / (sqrt(sqrt(count(blocks))) * sum(blocks))), 0) ) AS fragmentation
+       100 * (1 - nvl(sqrt(max(blocks) / (sqrt(sqrt(count(blocks))) * sum(blocks))), 0)) AS fragmentation
 
   FROM dba_free_space
 
