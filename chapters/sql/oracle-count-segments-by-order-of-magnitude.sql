ba80f949961599d1e684fae702dc92988b6f0eb0
Roberto Reale <roberto@reale.me>
2018-09-27 09:56:30 +0200

    Reformat SQL snippet

--- chapters/sql/oracle-count-segments-by-order-of-magnitude.sql
+++ chapters/sql/oracle-count-segments-by-order-of-magnitude.sql
@@ -1,13 +1,4 @@
-### COUNT number OF segments
-
-   FOR EACH
-
- ORDER OF magnitude * keywords*: decode
-FUNCTION,
-
-       analytic functions iec prefixes ARE used.
-SELECT decode( trunc(log(1024, bytes)), -- 0, 'bytes',
- 1, 'KiB', 2, 'MiB', 3, 'GiB', 4, 'TiB', 5, 'PiB', 6, 'EiB', 7, 'ZiB', 8, 'YiB', 'UNKNOWN') AS order_of_magnitude,
+SELECT decode(trunc(log(1024, bytes)), 0, 'bytes', 1, 'KiB', 2, 'MiB', 3, 'GiB', 4, 'TiB', 5, 'PiB', 6, 'EiB', 7, 'ZiB', 8, 'YiB', 'UNKNOWN') AS order_of_magnitude,
 
        count(*) COUNT
 
